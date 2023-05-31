81c66abde3b44b86e22a24a0e97a0a36b5829266
DilaraMammadli <124633385+DilaraMammadli@users.noreply.github.com>
2023-05-31 10:02:13 +0400

    Update urls.py

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,12 +1,11 @@
 
 from django.contrib import admin
-from django.urls import path
-#,include
-from hometask.views import product_list_view, product_detail_view
+from django.urls import path, include
+# from hometask.views import product_list_view, product_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path("", include("hometask.urls"))
-    path("list/", product_list_view),
-    path("detail/<id>", product_detail_view),
+    path("", include("hometask.urls"))
+#     path("list/", product_list_view),
+#     path("detail/<id>", product_detail_view),
 ]
